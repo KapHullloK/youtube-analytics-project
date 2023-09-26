b4be47e0b58f6cf2eb56f226c7b7dfe1012ee566
afro_0 <kirillov2136@gmail.com>
2023-09-26 10:45:10 +0300

    hw2

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,12 @@
 from src.channel import Channel
+from pprint import pprint
 
 if __name__ == '__main__':
     moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-    moscowpython.print_info()
+    print(moscowpython.title)
+    print(moscowpython.description)
+    print(moscowpython.channel_id)
+    print(moscowpython.url)
+    print(moscowpython.subscribers)
+    print(moscowpython.video_count)
+    print(moscowpython.views)
