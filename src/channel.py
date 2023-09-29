6ce642cb88db60cd1b5110eb5fb9f3e5559f022a
afro_0 <kirillov2136@gmail.com>
2023-09-29 14:54:04 +0300

    HW3

--- src/channel.py
+++ src/channel.py
@@ -10,17 +10,44 @@ API_KEY = os.getenv('YT_API')
 
 class Channel:
     def __init__(self, channel_id: str) -> None:
-        self.__channel_id = channel_id
+        self._channel_id = channel_id
 
         self._youtube = build('youtube', 'v3', developerKey=API_KEY)
-        self._channel = self._youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self._channel = self._youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
 
         self.title = self._channel["items"][0]["snippet"]["title"]
         self.description = self._channel["items"][0]["snippet"]["description"]
         self.url = "https://www.youtube.com/" + self._channel["items"][0]["snippet"]["customUrl"]
-        self.subscribers = self._channel["items"][0]["statistics"]["subscriberCount"]
+        self.subscribers = int(self._channel["items"][0]["statistics"]["subscriberCount"])
         self.video_count = self._channel["items"][0]["statistics"]["videoCount"]
         self.views = self._channel["items"][0]["statistics"]["viewCount"]
+
+    def __str__(self):
+        return f"{self.title} (https://www.youtube.com/channel/{self._channel_id})"
+
+    def __add__(self, other):
+        return self.subscribers + other.subscribers
+
+    def __sub__(self, other):
+        return self.subscribers - other.subscribers
+
+    def __mul__(self, other):
+        return self.subscribers * other.subscribers
+
+    def __truediv__(self, other):
+        return self.subscribers / other.subscribers
+
+    def __lt__(self, other):
+        return self.subscribers < other.subscribers
+
+    def __le__(self, other):
+        return self.subscribers <= other.subscribers
+
+    def __gt__(self, other):
+        return self.subscribers > other.subscribers
+
+    def __ge__(self, other):
+        return self.subscribers >= other.subscribers
 
     @classmethod
     def get_service(cls):
@@ -39,7 +66,7 @@ class Channel:
             existing_data = []
 
         existing_data.append({
-            "channel_id": self.__channel_id,
+            "channel_id": self._channel_id,
             "title": self.title,
             "description": self.description,
             "url": self.url,
